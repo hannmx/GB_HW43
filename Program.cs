cc96f12c6ff4e17e10aefc6d27c84843146a38e6
hannmx <mxkud@mail.ru>
2022-11-30 11:27:50 +0300

    Готовый код программы и добавлены файлы на отслеживание в git

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b1 - b2)/(k2 - k1);
+double y = k1 * x + b1;
+ Console.Write($"Точка пересечения прямых: A({x} , {y})");
